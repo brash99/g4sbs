a3d7f35cadbd9e1f19bc073f50922dc343e94236
Edward Brash <brash@jlab.org>
2024-09-06 14:45:58 -0400

    Updates

--- runscripts/checkjobs_jlab.sh
+++ runscripts/checkjobs_jlab.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo 'Running jobs:'
-squeue -u brash | grep R | grep g4sbs | wc -l
+squeue -u $USER | grep R | grep g4sbs | wc -l
 echo 'Pending jobs:'
-squeue -u brash | grep PD | grep g4sbs | wc -l
+squeue -u $USER | grep PD | grep g4sbs | wc -l
